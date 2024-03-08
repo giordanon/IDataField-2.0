477db364d38b390a250463f7b6941a53a5d47a27
Nico Giordano <ngiordano@ksu.edu>
2024-03-08 08:48:41 -0600

    upd welcome and gitignore

--- welcome.py
+++ welcome.py
@@ -3,3 +3,5 @@ def app():
     st.write("Welcome to IDataField")
     
     st.write("For issues when running the app reach to Nico or Jorge")
+    
+    st.write('Contact: ngiordano@ksu.edu - Phone: (785) 473-8442')
