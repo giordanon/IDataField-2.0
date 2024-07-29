ee6e257102136929fb31d6e20903972e2f6a9127
JorgeRomero1 <106196874+JorgeRomero1@users.noreply.github.com>
2024-07-29 08:41:07 -0500

    Canopy Cover
    New function for taking and processing the canopy cover photos.

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-import activities, dataupload, dataupload_other_data, labels, canopy_c, combinedata, seeds, merge_data, labels_stakes, welcome, field_data_uploader, metadata, combine
+import activities, dataupload, dataupload_other_data, labels, canopy_c, combinedata, seeds, merge_data, labels_stakes, welcome, field_data_uploader, assgin_coor_to_plots, Image_cut, flight_path_generator, rename_photos, Canopy, Protein, canopy_cover
 import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
 
 im = Image.open('content/wheat.png')
-st.set_page_config(page_title = "IDataField 2.0", page_icon = im)
+#st.set_page_config(page_title="IDataField 2.0", page_icon = im)
 
 PAGES = {  
     'Welcome!': welcome,
     'Activities Uploader': activities,
-    'Partitioning Data Uploader': dataupload,
+    'Partitioning Data Uploader': dataupload ,
     'Field Data Uploader': field_data_uploader,
     'Lab Data Uploader': dataupload_other_data,
     'Bag Labels Generator': labels, 
     'Stakes Labels Generator': labels_stakes,
-    'Canopy Cover Uploader': canopy_c, 
+    'Assign Plot Coordinates': assgin_coor_to_plots,
+    'Canopy Cover Uploader': Canopy, 
     'Seed Counter': seeds, 
-    'Combine': combine, 
-    'Merge Data': merge_data, 
-    'Meta-Data': metadata
+    'Combine Data Uploader': combinedata, 
+    'Merge Data': merge_data,
+    'Protein' : Protein,
+    'Canopy Cover' : canopy_cover
+    
 }
 #st.write(st.__version__) # Line to check streamlit version
 # Check sync
@@ -28,7 +31,7 @@ with st.sidebar:
     selection = option_menu(menu_title = "Menu", 
                             options = list(PAGES.keys()), 
                             icons = ['house', 'box-arrow-in-down','box-arrow-in-down','box-arrow-in-down','box-arrow-in-down',
-                                    'qr-code','qr-code', 'badge-cc-fill', 'upc-scan', 'box-arrow-in-down', 'union'], 
+                                    'qr-code','qr-code', 'pin-map', 'upc-scan', 'box-arrow-in-down', 'union'], 
                             menu_icon = 'three-dots'
                            )
 page = PAGES[selection]
